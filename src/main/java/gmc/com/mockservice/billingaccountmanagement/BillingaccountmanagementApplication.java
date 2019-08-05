f57a4b3f4fbc60bb2f5603d72c7df513f21044ea
Raya <rayameha@gmail.com>
2019-08-05 12:00:14 -0400

    initial commit

---
+++ src/main/java/gmc/com/mockservice/billingaccountmanagement/BillingaccountmanagementApplication.java
@@ -0,0 +1,13 @@
+package gmc.com.mockservice.billingaccountmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingaccountmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingaccountmanagementApplication.class, args);
+	}
+
+}
